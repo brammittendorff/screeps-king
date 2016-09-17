ab875954e665a5a68d43b2392583417b5e5ddcaa
Bram <bram@hack.fish>
2016-09-17 17:18:37 +0200

    Fixed issue with comparing of objects in source

--- dist/select.resource.js
+++ dist/select.resource.js
@@ -2,20 +2,20 @@ module.exports = {
 
     selectClosestTo: function(entity) {
         var source = entity.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-        //console.log(source.id);
         return source.id;
     },
 
     selectSecondClosestTo: function(entity) {
         var sources = entity.room.find(FIND_SOURCES);
         var exclude = entity.pos.findClosestByPath(FIND_SOURCES);
-        var source = _.find(sources, function(source) { return source != exclude });
-        
-        if(!source.id) {
-            exclude.id = source.id
+        var source = _.find(sources, function(source) {
+            return source.id != exclude.id
+        });
+
+        if(!source) {
+            exclude.id = source
         }
-        //console.log(source.id);
-        return source.id;
+        return source;
     }
 
 };
